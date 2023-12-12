7af6583200eca7d458d706d3b210dff1bce08a41
laila <6235702@edu.vaniercollege.qc.ca>
2023-12-11 23:36:46 -0500

    spaces

--- src/main/java/org/ghoujal/Main.java
+++ src/main/java/org/ghoujal/Main.java
@@ -11,13 +11,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
         SchoolManagementSystem s1 = new SchoolManagementSystem("Vanier");
 
         s1.addDepartment("Compsci");
         s1.printDepartments();
-
         s1.addCourse("Intro to prog", 3, "D001");
         s1.addCourse("Intro to prog 2", 3, "D001");
         s1.addCourse("Advanced Programming", 3, "D001");
@@ -25,7 +22,6 @@ public class Main {
         s1.addCourse("User Interface Fundamentals", 3, "D001");
         s1.addCourse("Algorithms", 4, "D001");
         s1.addCourse("Advanced Algorigthms", 5, "D001");
-
         s1.addStudent("Laila", "Ghoujal", "D001");
         s1.addStudent("Joe", "Ghoujal", "D001");
         s1.addStudent("Jane", "Ghoujal", "D001");
@@ -34,15 +30,11 @@ public class Main {
         s1.addStudent("Ted", "Ghoujal", "D001");
         s1.addStudent("Vishnu", "Ghoujal", "D001");
         s1.addStudent("Bob", "Ghoujal", "D001");
-
-
         Course c1 = s1.findCourse("C001");
-
         s1.findDepartment("D001");
         s1.printStudents();
         s1.addTeacher("Yi", "Wang", "D001");
         s1.addTeacher("Andre", "Cholmsky", "D001");
-
         s1.printTeachers();
         s1.modifyCourseTeacher("T001", "C001");
         s1.printCourses();
@@ -63,10 +55,5 @@ public class Main {
         s1.registerCourse("S001", "C005");
         s1.registerCourse("S001", "C006");
         s1.printCourses();
-
-
-
-
-
     }
 }
